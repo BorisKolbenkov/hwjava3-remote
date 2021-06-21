376c13960ea0f7bd34184b83527d990e1633d7f9
BorisKolbenkov <kolbenkov@gmail.com>
2021-06-21 16:26:51 +0300

    Initial commit

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 10000;
         int bonus = 20;
-        int mile = 10000 / 20;
+        int mile = price / 20;
         System.out.println(mile);
     }
 }
